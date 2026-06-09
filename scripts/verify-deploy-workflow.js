298154fafda0cfdbd1d3b80e5a3d08f37fd6e155
neonwatty <jermwatt@gmail.com>
2026-06-08 17:51:27 -0700

    ci: support hosted github app deploy secrets

--- scripts/verify-deploy-workflow.js
+++ scripts/verify-deploy-workflow.js
@@ -12,13 +12,18 @@ const required = [
   'wrangler_environment:',
   'smoke_cors_disallowed_origin:',
   'GITHUB_DEPLOY_ENVIRONMENT: ${{ inputs.environment }}',
+  'WORKER_GITHUB_APP_ID: ${{ secrets.BOARD_GITHUB_APP_ID }}',
+  'WORKER_GITHUB_APP_PRIVATE_KEY: ${{ secrets.BOARD_GITHUB_APP_PRIVATE_KEY }}',
   'SMOKE_CORS_DISALLOWED_ORIGIN: ${{ inputs.smoke_cors_disallowed_origin }}',
+  'BOARD_GITHUB_APP_ID and BOARD_GITHUB_APP_PRIVATE_KEY must be set together',
   'resolved_environment="$WRANGLER_ENVIRONMENT"',
   'resolved_environment="$GITHUB_DEPLOY_ENVIRONMENT"',
   'echo "WRANGLER_ENVIRONMENT=$resolved_environment" >> "$GITHUB_ENV"',
   'Production deploys must use wrangler_environment=production',
   'Production deploys require smoke_url',
   '--cors-disallowed-origin "$SMOKE_CORS_DISALLOWED_ORIGIN"',
+  'secrets.GITHUB_APP_ID = process.env.WORKER_GITHUB_APP_ID',
+  "writeFileSync('.deploy.secrets', `${JSON.stringify(secrets, null, 2)}\\n`)",
   'rm -f .deploy.secrets',
 ];
 
